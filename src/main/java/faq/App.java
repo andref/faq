f13b25daf4332995ac24a8924d6b09786bb70d9d
André Fillipe <andre@filli.pe>
2016-10-27 13:02:06 -0200

    Inicializa o Hibernate na aplicação

--- src/main/java/faq/App.java
+++ src/main/java/faq/App.java
@@ -1,10 +1,25 @@
 package faq;
 
-import io.dropwizard.Application;
-import io.dropwizard.setup.Environment;
 import faq.resources.QuestaoResource;
+import io.dropwizard.Application;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.hibernate.ScanningHibernateBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 
 public class App extends Application<AppConfig> {
+
+    public ScanningHibernateBundle<AppConfig> hibernate = new ScanningHibernateBundle<AppConfig>("faq.core") {
+        @Override
+        public PooledDataSourceFactory getDataSourceFactory(AppConfig configuration) {
+            return configuration.getDatabase();
+        }
+    };
+
+    @Override
+    public void initialize(Bootstrap<AppConfig> bootstrap) {
+        bootstrap.addBundle(hibernate);
+    }
 
     @Override
     public void run(AppConfig configuration, Environment environment) throws Exception {
