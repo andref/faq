11bb9ea415ecec4c2d7492608c24c5d4f37b8636
André Fillipe <andre@filli.pe>
2016-11-02 14:29:55 -0200

    Remove Optional dos TOs
    Optional é pra ser usado como valor de retorno de métodos. Sem
    optional, o código fica mais simples.

--- src/main/java/faq/api/ItemComId.java
+++ src/main/java/faq/api/ItemComId.java
@@ -1,7 +1,7 @@
 package faq.api;
 
 import javax.validation.constraints.NotNull;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.UUID;
 
 public class ItemComId {
@@ -9,18 +9,17 @@ public class ItemComId {
     @NotNull
     public UUID id;
 
-    public Optional<String> descricao;
+    public String descricao;
 
     ItemComId() {
     }
 
     public ItemComId(UUID id) {
-        this.id = id;
-        this.descricao = Optional.empty();
+        this(id, null);
     }
 
     public ItemComId(UUID id, String descricao) {
-        this.id = id;
-        this.descricao = Optional.ofNullable(descricao);
+        this.id = Objects.requireNonNull(id);
+        this.descricao = descricao;
     }
 }
--- src/main/java/faq/api/QuestaoTO.java
+++ src/main/java/faq/api/QuestaoTO.java
@@ -1,55 +1,47 @@
 package faq.api;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import faq.core.Questao;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.valuehandling.UnwrapValidatedValue;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.Optional;
 import java.util.UUID;
 
 public class QuestaoTO {
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public UUID id;
 
     @NotBlank
     @NotNull
     @Size(max = 1000)
-    @JsonProperty
     public String pergunta;
 
     @NotBlank
     @NotNull
     @Size(max = 10000)
-    @JsonProperty
     public String resposta;
 
     @Size(max = 500)
-    @UnwrapValidatedValue
-    public Optional<String> autor = Optional.empty();
+    public String autor;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public LocalDate dataDePublicacao;
 
-    QuestaoTO() {
+    public QuestaoTO() {
     }
 
     public QuestaoTO(Questao questao) {
         id = questao.getId();
         pergunta = questao.getPergunta();
         resposta = questao.getResposta();
-        autor = questao.getAutor();
+        questao.getAutor().ifPresent(autor -> this.autor = autor);
         dataDePublicacao = questao.getDataDePublicacao();
     }
 
     public Questao atualizar(Questao questao) {
         questao.setPergunta(pergunta);
         questao.setResposta(resposta);
-        autor.ifPresent(questao::setAutor);
+        questao.setAutor(autor);
         return questao;
     }
 }
